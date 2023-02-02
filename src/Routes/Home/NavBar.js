f829255130ff975c622d536c94cf883417a712a1
Richard Eberechi Uche <eb3rechi@gmail.com>
2023-02-02 13:08:10 +0100

    fix responsiveness

--- src/Routes/Home/NavBar.js
+++ src/Routes/Home/NavBar.js
@@ -20,6 +20,19 @@ export function NavBar() {
         </div>
       </nav>
       <Outlet></Outlet>
+      <div className="pd-10 bg-dark">
+        <div>
+          <h4 className="mg-0 white fnt-md">contact</h4>
+          <ul className="mg-0 li-none pd-0">
+            <li>number</li>
+            <li>info@company.com</li>
+            <li>location</li>
+          </ul>
+        </div>
+        <div className="fl fl-center">
+          <p>&copy; copyright 2023 ACOA</p>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,51 @@
 import { Layout } from "./Section";
 import "../components/component.syles.scss";
+import imageLg from "../assets/images/beefImage.png";
 
 export default function Footer() {
   return (
     <Layout title={"Services and Event"}>
       <div className="gr">
-        <ul className="fl">
-          <li>instagram</li>
-          <li>FaceBook</li>
-          <li>whatsapp</li>
+        <ul className="li-flex">
+          <li className="mg-10">instagram</li>
+          <li className="mg-10">FaceBook</li>
+          <li className="mg-10">whatsapp</li>
         </ul>
+      </div>
+      <div>
+        <div className="image-container">
+          <img src={imageLg} alt="raw beef and natural spice"></img>
+        </div>
+      </div>
+      <div className="gr txt-start mg-10">
+        <div>
+          <h3 className="mg-bottom-5"> All Natural feeds</h3>
+          <div className=" gr pd-10">
+            <div className="gr-item">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui
+                voluptate quaerat accusamus perferendis ratione quae!
+              </p>
+            </div>
+            <div className="gr-item">
+              <ul className="pd-0 li-none">
+                <li className="mg-bottom-5">
+                  Lorem, ipsum dolor sit amet consectetur
+                </li>
+                <li className="mg-bottom-5">
+                  Lorem, ipsum dolor sit amet consectetur
+                </li>
+                <li className="mg-bottom-5">
+                  Lorem, ipsum dolor sit amet consectetur
+                </li>
+                <li className="mg-bottom-5">
+                  Lorem, ipsum dolor sit amet consectetur
+                </li>
+              </ul>
+            </div>
+          </div>
+          <button className="btn btn-primary mg-top-15"> Order Now</button>
+        </div>
       </div>
     </Layout>
   );
